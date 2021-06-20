a29e6534781607b8c88427ec727d9d0dfb1e4a93
Shyrokyi Vladislav <vlad16062001@gmail.com>
2021-06-20 21:56:43 +0300

    added sending message system by response on user message

---
+++ TemplateConsoleApp/MessageSystem/ExampleMessageHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using TemplateLib.Factory;
+using TemplateLib.Objects;
+
+namespace TemplateConsoleApp.MessageSystem
+{
+    internal class ExampleMessageHandler : IMessageHandler
+    {
+        public Task<Message> CreateMessage(ITelegramBotClient botClient, CancellationToken token, Update update)
+        {
+            const string user = "USER";
+            const string text = "TEXT";
+            const string time = "TIME";
+            const string append = "APPEND";
+
+            var headBlock = new TextBlock($"User: %[{user}]%%[{append}]%");
+            headBlock.PutVariable(user, update.Message.Chat.Username);
+            headBlock.PutVariable(append, $" ({update.Message.Chat.LastName} {update.Message.Chat.FirstName})");
+
+            var bodyBlock = new TextBlock($"Text: %[{text}]%%[{append}]%");
+            bodyBlock.PutVariable(text, update.Message.Text);
+
+            var bottomBlock = new TextBlock($"Time: %[{time}]%%[{append}]%");
+            bottomBlock.PutVariable(time, update.Message.Date.ToString(CultureInfo.CurrentCulture));
+
+            var block = TextBlockFactory.CreateText("DYN", "\n", headBlock, bodyBlock, bottomBlock);
+
+            return botClient.SendTextMessageAsync(update.Message.Chat, block.WriteWithEditor(), cancellationToken: token);
+        }
+    }
+}
---
+++ TemplateConsoleApp/MessageSystem/IMessageHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TemplateConsoleApp.MessageSystem
+{
+    public interface IMessageHandler
+    {
+        Task<Message> CreateMessage(ITelegramBotClient botClient, CancellationToken token, Update update);
+    }
+}
