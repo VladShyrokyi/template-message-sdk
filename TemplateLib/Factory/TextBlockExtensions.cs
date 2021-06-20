a48c9e873dcb22b183768ced74ed73f1dd499745
Shyrokyi Vladislav <vlad16062001@gmail.com>
2021-06-20 20:41:49 +0300

    added TextBlockExtensions.cs

---
+++ TemplateLib/Factory/TextBlockExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TemplateLib.Objects;
+
+namespace TemplateLib.Factory
+{
+    public static class TextBlockExtensions
+    {
+        public static TextBlock Merge(this IEnumerable<TextBlock> blocks, string dynamicVariableName, string separator)
+        {
+            return TextBlockFactory.CreateText(dynamicVariableName, separator, blocks.ToArray());
+        }
+    }
+}
