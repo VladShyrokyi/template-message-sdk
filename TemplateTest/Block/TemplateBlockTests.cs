b4d4e3a0a4115e78c9bcaf6a7e440df9c22a91e8
Shyrokyi Vladislav <vlad16062001@gmail.com>
2021-08-07 14:53:51 +0300

    [ADD] Template tests

---
+++ TemplateTest/Block/TemplateBlockTests.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using TemplateLib;
+using TemplateLib.Block;
+using TemplateLib.Editor;
+using TemplateLib.Exception;
+using TemplateLib.Writer;
+
+namespace TemplateTest.Block
+{
+    [TestFixture(TestOf = typeof(TemplateBlock))]
+    public class TemplateBlockTests
+    {
+        [Test]
+        public void Write_template_with_writer([Values("", "Template")] string template)
+        {
+            // Arrange
+            var block = CreateBlockWithoutVariables(template);
+
+            // Act
+            var text = block.Write();
+            var textWithoutEditor = block.WriteWithoutEditor();
+
+            // Assert
+            Assert.AreEqual(text, template);
+            Assert.AreEqual(textWithoutEditor, template);
+        }
+
+
+        [Test]
+        public void Write_template_with_writer_and_editor([Values("", "Template")] string template,
+                                                          [Values(null, "", "[start]")] string start,
+                                                          [Values(null, "", "[end]")] string end)
+        {
+            // Arrange
+            var editor = new WrapperEditor(start, end);
+            var inverseEditor = new WrapperEditor(end, start);
+            var block = CreateBlockWithoutVariables(template, editor);
+
+            // Act
+            var text = block.Write();
+            var textWithEditor = block.WriteWithEditor(inverseEditor);
+            var textWithoutEditor = block.WriteWithoutEditor();
+
+            // Assert
+            Assert.AreEqual(textWithoutEditor, template);
+            Assert.AreEqual(textWithEditor, end + template + start);
+            Assert.AreEqual(text, start + template + end);
+        }
+
+        [Theory]
+        public void Throws_when_get_non_added_variable([Values(null, "", "0", "1", "VAR")] string variableName)
+        {
+            // Arrange
+            var block = CreateBlockWithoutVariables("");
+
+            // Act
+            TestDelegate getVariable = () => block.GetVariable(variableName);
+
+            // Assert
+            Assert.That(getVariable,
+                Throws.Exception
+                    .TypeOf<KeyNotFoundException>()
+                    .Or
+                    .TypeOf<VariableNameNullException>()
+            );
+        }
+
+        [Test]
+        public void Copy_template([Values("", "Template")] string template)
+        {
+            // Arrange
+            const string addedTemplate = "added";
+            const string prepend = "[start]";
+            const string append = "[end]";
+
+            // Act
+            var editor = new WrapperEditor(prepend, append);
+            var block = CreateBlockWithoutVariables(template, editor);
+            var copy = block.Copy();
+
+            // Assert
+            Assert.AreEqual(copy, block);
+            Assert.AreEqual(copy.Write(), prepend + template + append);
+            Assert.AreEqual(copy.WriteWithEditor(editor), prepend + template + append);
+            Assert.AreEqual(copy.WriteWithoutEditor(), template);
+
+            // Act 2
+            block.Append(addedTemplate);
+
+            // Assert
+            Assert.IsFalse(Equals(copy, block));
+            Assert.IsFalse(Equals(copy.Write(), block.Write()));
+            Assert.IsFalse(Equals(copy.WriteWithEditor(editor), block.WriteWithEditor(editor)));
+            Assert.IsFalse(Equals(copy.WriteWithoutEditor(), block.WriteWithoutEditor()));
+        }
+
+        protected static TemplateBlock CreateBlockWithoutVariables(string template, ITextEditor editor = null)
+        {
+            var writer = new RegexTextWriter(template, DefaultRegex.Regex, DefaultRegex.SelectorFactory);
+            return new TemplateBlock(writer, editor);
+        }
+
+        protected static TemplateBlock CreateBlockWithVariables(string template,
+                                                                Dictionary<string, ITextBlock> variables,
+                                                                ITextEditor editor = null)
+        {
+            var writer = new RegexTextWriter(template, DefaultRegex.Regex, DefaultRegex.SelectorFactory);
+            var block = new TemplateBlock(writer, editor);
+            foreach (var pair in variables)
+                block.PutVariable(pair.Key, pair.Value);
+            return block;
+        }
+    }
+}
---
+++ TemplateTest/Template/RegexWriterTests.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using NUnit.Framework;
+
+using TemplateLib;
+using TemplateLib.Writer;
+
+namespace TemplateTest.Template
+{
+    [TestFixture(TestOf = typeof(RegexTextWriter))]
+    public class RegexWriterTests
+    {
+        private static object[] Regex => new object[]
+        {
+            new object[] {DefaultRegex.Regex, DefaultRegex.SelectorFactory},
+            new object[] {"\\{\\{([^\\{\\},\\s]*)\\}\\}", new Func<string, string>(name => $"{{{{{name}}}}}")}
+        };
+
+        private static Dictionary<string, string>[] Variables => new[]
+        {
+            new Dictionary<string, string>
+            {
+                {"VAR", ""}
+            },
+            new Dictionary<string, string>
+            {
+                {"VAR_1", ""},
+                {"VAR_2", "Test"},
+                {"VAR_3", "Test 1"},
+                {"VAR_4", "1"},
+                {"VAR_5", "Test $@#123!$"},
+                {"VAR_6", "Alert"}
+            }
+        };
+
+        [Test]
+        public void Create_selector([Values(null, "", "SELECTOR")] string selectorName)
+        {
+            // Arrange
+            var writer = new RegexTextWriter("", DefaultRegex.Regex, DefaultRegex.SelectorFactory);
+
+            // Act
+            var selector = writer.CreateSelector(selectorName);
+
+            // Assert
+            Assert.AreEqual(selector, DefaultRegex.SelectorFactory(selectorName));
+        }
+
+        [TestCaseSource(nameof(Regex))]
+        public void Not_found_selectors_in_template(string regex, Func<string, string> selectorFactory)
+        {
+            // Arrange
+            const string template = "Template";
+            const string variableName = "VAR";
+            const string variableValue = "Text";
+            var variables = new Dictionary<string, string> {{variableName, variableValue}};
+
+            // Act
+            var writer = new RegexTextWriter(template, regex, selectorFactory);
+            var text = writer.ToWriting(variables);
+
+            // Assert
+            Assert.AreEqual(writer.Template, template);
+            Assert.IsEmpty(writer.Selectors);
+            StringAssert.DoesNotContain(variableName, text);
+            StringAssert.DoesNotContain(variableValue, text);
+        }
+
+        [TestCaseSource(nameof(Regex))]
+        public void Found_selectors_in_template(string regex, Func<string, string> selectorFactory)
+        {
+            // Arrange
+            const string variableName = "SELECTOR";
+            const string variableValue = "Text";
+            var variables = new Dictionary<string, string> {{variableName, variableValue}};
+            var variableNameToSelector = variables.ToDictionary(pair => pair.Key, pair => selectorFactory(pair.Key));
+            var template = CreateTextWithVariables(variableNameToSelector);
+
+            // Act
+            var writer = new RegexTextWriter(template, regex, selectorFactory);
+            var text = writer.ToWriting(variables);
+
+            // Assert
+            Assert.AreEqual(writer.Template, template);
+            Assert.IsNotEmpty(writer.Selectors);
+            CollectionAssert.Contains(writer.Selectors, variableName);
+            StringAssert.Contains(variableName, text);
+            StringAssert.Contains(variableValue, text);
+        }
+
+        [Test]
+        public void Write_text_with_default_variable([Values(null, "", "Default")] string variableValue)
+        {
+            // Arrange
+            const string variableName = "DEFAULT";
+            var variables = new Dictionary<string, string>
+            {
+                {variableName, variableValue}
+            };
+            var variableNameToSelector =
+                variables.ToDictionary(pair => pair.Key, pair => DefaultRegex.SelectorFactory(pair.Key));
+            var template = CreateTextWithVariables(variableNameToSelector);
+
+            // Act
+            var writer = new RegexTextWriter(template, DefaultRegex.Regex, DefaultRegex.SelectorFactory);
+            var text = writer.ToWriting(new Dictionary<string, string>(), variableValue);
+
+            // Assert
+            var result = CreateTextWithVariables(variables);
+            Assert.AreEqual(text, result);
+        }
+
+        [TestCaseSource(nameof(Variables))]
+        public void Write_text_with_variables(Dictionary<string, string> variables)
+        {
+            // Arrange
+            var variableNameToSelector = variables
+                .ToDictionary(pair => pair.Key, pair => DefaultRegex.SelectorFactory(pair.Key));
+            var template = CreateTextWithVariables(variableNameToSelector);
+
+            // Act
+            var writer = new RegexTextWriter(template, DefaultRegex.Regex, DefaultRegex.SelectorFactory);
+            var text = writer.ToWriting(variables);
+
+            // Assert
+            Assert.AreEqual(writer.Template, template);
+            Assert.IsNotEmpty(writer.Selectors);
+            Assert.AreEqual(writer.Selectors.Count, variables.Count);
+
+            var result = CreateTextWithVariables(variables);
+            Assert.AreEqual(text, result);
+        }
+
+        [TestCaseSource(nameof(Variables))]
+        public void Copy_writer(Dictionary<string, string> variables)
+        {
+            //Arrange
+            const string defaultValue = "DEFAULT";
+            var variableNameToSelector =
+                variables.ToDictionary(pair => pair.Key, pair => DefaultRegex.SelectorFactory(pair.Key));
+            var template = CreateTextWithVariables(variableNameToSelector);
+
+            // Act
+            var writer = new RegexTextWriter(template, DefaultRegex.Regex, DefaultRegex.SelectorFactory);
+            var copy = writer.Copy();
+
+            // Assert
+            Assert.AreEqual(copy.Template, writer.Template);
+            Assert.AreEqual(copy.ToWriting(variables), writer.ToWriting(variables));
+            Assert.AreEqual(copy.ToWriting(variables, defaultValue), writer.ToWriting(variables, defaultValue));
+            Assert.That(copy, Is.AssignableTo(writer.GetType()));
+            Assert.AreEqual(copy, writer);
+        }
+
+        private static string CreateTextWithVariables(Dictionary<string, string> variables)
+        {
+            return variables.Aggregate("Text:",
+                (container, pair) => container + $"\n - variable {pair.Key} = {pair.Value}"
+            );
+        }
+    }
+}
